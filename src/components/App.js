6ca08c7cf6817ae57ff349f95cd40688efd7fdf6
pankaj819kumar <pankaj819kumar@gmail.com>
2022-10-11 12:04:35 +0530

    add reducers

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,24 @@
+import Navbar from './Navbar';
+import { data } from '../data';
+import MovieCard from './MovieCard';
 
 
 function App() {
   return (
     <div className="App">
-      Project Setup
+      <Navbar />
+      <div className="main">
+        <div className="tabs">
+          <div className="tab">Movies</div>
+          <div className="tab">Favourites</div>
+        </div>
+
+        <div className="list">
+          {data.map((movie) => {
+            return <MovieCard movie={movie}/>
+          })}
+        </div>
+      </div>
     </div>
   );
 }
