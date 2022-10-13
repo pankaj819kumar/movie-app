8c8cc6fa0b3cc4c4b3cc0f9a8e6aab63168922b8
Pankaj Kumar <pankaj819kumar@gmail.com>
2022-10-13 17:05:02 +0530

    add functionality of favourite, unfavourite movie and change tabs

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,3 +5,39 @@
 // {
 //     type: 'DECREASE_COUNT'
 // }
+
+// action types
+export const ADD_MOVIES = 'ADD_MOVIES';
+export const ADD_TO_FAVOURITES = 'ADD_TO_FAVOURITES';
+export const REMOVE_FROM_FAVOURITES = 'REMOVE_FROM_FAVOURITES';
+export const SET_SHOW_FAVOURITES = 'SET_SHOW_FAVOURITES';
+
+// action creators
+export function addMovies(movies, id) {
+    return {
+        type: ADD_MOVIES,
+        movies: movies,
+        id
+    }
+}
+
+export function addFavourite(movie) {
+    return {
+        type: ADD_TO_FAVOURITES,
+        movie: movie,
+    }
+}
+
+export function removeFromFavourites(movie) {
+    return {
+        type: REMOVE_FROM_FAVOURITES,
+        movie: movie
+    }
+} 
+
+export function setShowFavourites(val) {
+    return {
+        type: SET_SHOW_FAVOURITES,
+        val     // short for val : val
+    }
+} 
--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,70 @@
 import Navbar from './Navbar';
+import React from 'react';
 import { data } from '../data';
 import MovieCard from './MovieCard';
+import { addMovies, setShowFavourites } from '../actions';
 
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <div className="main">
-        <div className="tabs">
-          <div className="tab">Movies</div>
-          <div className="tab">Favourites</div>
-        </div>
+class App extends React.Component {
 
-        <div className="list">
-          {data.map((movie) => {
-            return <MovieCard movie={movie}/>
-          })}
+  // invoked immediately after a component is mounted
+  componentDidMount() {
+    const { store } = this.props;
+
+
+    // Adds a change listener. It will be called any time an action is dispatched
+    store.subscribe(() => {
+      console.log("UPDATED (in subscribe)");
+      this.forceUpdate();  
+    });
+      
+    // make api call
+    // dispatch action
+    store.dispatch(addMovies(data));
+    console.log("STATE", this.props.store.getState());
+  }
+  isMovieFavourite = (movie) => {
+    const { favourites } = this.props.store.getState();
+
+    const index = favourites.indexOf(movie);
+
+    if (index !== -1) {
+      //found the movie
+      return true;
+    }
+    return false;
+  }
+  onChangeTab = (val) => {
+    this.props.store.dispatch(setShowFavourites(val));
+  }
+  render() {
+    const { list, favourites, showFavourites } = this.props.store.getState(); // {list []  , favourites [] } (destructuring)
+    console.log('RENDER', this.props.store.getState()); 
+    const displayMovies = showFavourites ? favourites : list;
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="main">
+          <div className="tabs">
+            <div className={`tab ${showFavourites? '' : 'active-tabs'}`} onClick={() => this.onChangeTab(false)}>Movies</div>
+            <div className={`tab ${showFavourites? 'active-tabs' : ''}`} onClick={() => this.onChangeTab(true)}>Favourites</div>
+          </div>
+
+          <div className="list">
+            {displayMovies.map((movie, index) => (   // automatically return
+              <MovieCard
+                movie={movie}
+                key={`movies-${index}`}
+                dispatch={this.props.store.dispatch}
+                isFavourite={this.isMovieFavourite(movie)}
+              />
+            ))}
+          </div>
+          {displayMovies.length === 0 ? <div className="no-movies">No movies to display!</div> : null}
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
